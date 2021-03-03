812f0b8bb87db0288334e42ebd7ee2e84eecb92f
Shaqeel Less <54390756+MogammadShaqeelless16@users.noreply.github.com>
2021-03-03 02:56:35 -0800

    Update geolocation.cs

--- ECD-Verifi/ECD-Verifi/geolocation.cs
+++ ECD-Verifi/ECD-Verifi/geolocation.cs
@@ -1,2 +1,24 @@
 // tremaines code 
+using system; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
+using Xamarin.Forms;
+
+namespace LocationApp
+{
+  public class App : Application 
+  {
+   public App()
+   {
+     IntializeComponent();
+     
+     MainPage = new LocationApp.MainPage();
+     }
+     protected override void Onstart()
+     {
+     
+     proteted override void Onsleep()
+     }
+     
