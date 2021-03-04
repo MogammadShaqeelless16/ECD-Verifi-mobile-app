8e52bdbd9e2af1b19bb1ef244bdbc29fbaff1b87
shaqeel less <shaqeelless4@gmail.com>
2021-03-05 00:33:40 +0200

    Updated Splash Screen

--- ECD-Verifi/ECD-Verifi.Android/MainActivity.cs
+++ ECD-Verifi/ECD-Verifi.Android/MainActivity.cs
@@ -28,6 +28,7 @@ namespace ECD_Verifi.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            SetContentView(Resources.Layout.Target);
         }
     }
 }
