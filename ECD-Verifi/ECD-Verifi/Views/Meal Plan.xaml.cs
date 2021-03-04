f8490755e9e8295c9246849627f5292c5ef2b25f
shaqeel less <shaqeelless4@gmail.com>
2021-03-04 23:15:52 +0200

    Added content pages with regards to the wiresframes.

---
+++ ECD-Verifi/ECD-Verifi/Views/Meal Plan.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ECD-Verifi.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class Meal_Plan : ContentPage
+{
+    public Meal_Plan()
+    {
+        InitializeComponent();
+    }
+}
+}
