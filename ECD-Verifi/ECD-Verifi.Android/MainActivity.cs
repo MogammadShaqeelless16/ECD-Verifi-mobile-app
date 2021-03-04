89afe8b738fd024162e5d9cd8bfacceda7bf3787
shaqeel less <shaqeelless4@gmail.com>
2021-03-05 00:05:53 +0200

    Update - Layout

--- ECD-Verifi/ECD-Verifi.Android/MainActivity.cs
+++ ECD-Verifi/ECD-Verifi.Android/MainActivity.cs
@@ -9,8 +9,8 @@ using Android.OS;
 
 namespace ECD_Verifi.Droid
 {
-    [Activity(Label = "ECD_Verifi", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    [Activity(Label = "ECD_Verifi", Icon = "@mipmap/icon", Theme = "@style/MainTheme". MainLauncher = true, Icon = "@drawable/icon",Theme "@style/Theme.AppCompat.Light.NoActionBar" )]
+    public class TargetActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
